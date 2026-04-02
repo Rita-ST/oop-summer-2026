ab69f3f386a12338ee439d12f1d11b3398a289d4
Rita-36173 <londiwegadlela44@gmail.com>
2026-04-02 07:59:22 +0000

    lecture 5 practice

---
+++ student/36173/lecture_5_test.py
@@ -0,0 +1,16 @@
+#creating class and objects 
+class Chair:
+    wheels = 4
+
+    def __init__(self,location,colour,year):
+        self.location=location 
+        self.colour =colour
+        self.year = year 
+    def description(self):
+        return f"at {self.location} the chair is {self.colour} ,it was made in {self.year}"
+chair1=Chair("desk 1","blue",2025)
+chair2=Chair("desk 2","black",2024)
+chair4=Chair("desk3","red",2022)
+print(chair1.description())
+print(chair2.colour)
+print(chair4.description())
